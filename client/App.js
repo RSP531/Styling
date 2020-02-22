d9606a2eb971266c11ce84c50088ca7fe863a5b2
Rob Peschke <rspesc31@gmail.com>
2020-02-22 10:46:54 -0600

    stuff

--- client/App.js
+++ client/App.js
@@ -1,21 +1,73 @@
 import React from 'react';
-import GroceryItem from './GroceryItem.jsx'
+import GroceryItem from './GroceryItem.jsx';
+import $ from 'jquery';
+import axios from 'axios';
 
 export default class App extends React.Component{
   constructor(props){
-    super(props)
+    super()
     this.state = {
       groceryInput: '',
       groceryList: ['broccoli', 'queso', 'oreos']
     }
   }
 
+  componentDidMount(){
+    this.getInformation()
+  }
+
   deleteItem(event){
     let tempArry = this.state.groceryList
-    tempArry.splice(event.target.value,1)
+    tempArry.splice(event.target.value, 1)
     this.setState({
       groceryList: tempArry
     })
+  }
+
+  getInformation(){
+    //Three technologies for HTTP request: Axios, Fetch, AJAX
+    //axios get request
+    //DON'T FORGET TO IMPORT AXIOS
+    axios.get('http://jservice.io/api/clues')
+      .then( (response) => {
+        console.log(response)
+        this.setState({
+          question: response.data[0].question,
+          answer: response.data[0].answer
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    //FETCH
+    //Don't need to import anything
+    // fetch('http://jservice.io/api/clues')
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((myJson) => {
+    //     this.setState({
+    //       question: myJson[1].question,
+    //       answer: myJson[1].answer
+    //     })
+    //   });
+
+    //AJAX
+    //IMPORT & install jquery
+    //BARF 
+    $.ajax({
+      url: "http://jservice.io/api/clues",
+      type: "GET",
+      success: (result)=>{
+        this.setState({
+          question: result[2].question,
+          answer: result[2].answer
+        })
+      },
+    error: (error) => {
+      console.log(error)
+    }})
   }
 
   handleChange(event){
@@ -34,8 +86,6 @@ export default class App extends React.Component{
     })
   }
 
-
-
   render(){
     return (
       <div>
@@ -43,8 +93,12 @@ export default class App extends React.Component{
         <input onChange={this.handleChange.bind(this)} value={this.state.groceryInput}></input>
         <button onClick={this.submitItem.bind(this)}>Submit Item</button>
         {this.state.groceryList.map((item,index) => (
-          <GroceryItem key={index} indexValue={index} oneItem={item} deleteItem={this.deleteItem.bind(this)}/>
+              <GroceryItem key={index} indexValue={index} oneItem={item} deleteItem={this.deleteItem.bind(this)}/>
         ))}
+
+        <h1>It's Trivia Time</h1>
+        <div>Question:  {this.state.question}</div>
+        <div>Answer:  {this.state.answer}</div>
       </div>
     )
   }
--- client/GroceryItem.jsx
+++ client/GroceryItem.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 function GroceryItem(props) {
-  return (
-    <>
-      <li>{props.oneItem}</li>
-      <button value={props.indexValue} onClick={props.deleteItem}>Delete Item</button>
-    </>
-  )
+  if(props.oneItem==='jerky'){
+    return (
+      <li>
+        {props.oneItem} can never be deleted from your list
+      </li>
+    )
+  }
+    return(
+      <>
+        <li>{props.oneItem}</li>
+        <button value={props.indexValue} onClick={props.deleteItem}>Delete Item</button>
+      </>
+    )
 }
 
 export default GroceryItem;
